c4834ea48fd19f0a65535bcc3d2c7b00c9d7b1f2
David Cavazos <dcavazos@google.com>
2019-09-04 16:21:23 -0700

    Updated version to 0.1.5

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="html2md",
-    version="0.1.4",
+    version="0.1.5",
     author="David Cavazos",
     author_email="dcavazosw@gmail.com",
     description="HTML to Markdown converter.",
